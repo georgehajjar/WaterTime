d85cd9aa56e2399bb5350fdedd940a9c00dfa915
George Hajjar <georgehajjar@george-hajjars-macbook-pro.local>
2018-06-13 10:35:06 -0400

    Initial Commit

---
+++ waterBottleTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  waterBottleTimer
+//
+//  Created by George Hajjar on 2018-06-13.
+//  Copyright © 2018 George Hajjar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ waterBottleTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  waterBottleTimer
+//
+//  Created by George Hajjar on 2018-06-13.
+//  Copyright © 2018 George Hajjar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
